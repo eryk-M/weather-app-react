13ccdc2bf5111ccdfded9fb2b90fa8520f2f9186
Eryk <chiracle@gmail.com>
2019-03-14 22:37:40 +0100

    Updates

---
+++ precache-manifest.cf0954f224c22621dd4e50f61151850b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "41ffe824ece54fc878eb",
+    "url": "/weather-app-react/static/js/main.02c249bf.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/weather-app-react/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "41ffe824ece54fc878eb",
+    "url": "/weather-app-react/static/css/main.69efc1b0.chunk.css"
+  },
+  {
+    "revision": "f017cf9c44b40ae4a8a6dbf8870c7907",
+    "url": "/weather-app-react/index.html"
+  }
+];
